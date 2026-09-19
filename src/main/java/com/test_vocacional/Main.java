b91ceb6d2d68ef3bcac271bbd9a13ce5555e020d
BenjaminRC-TESCH <benj.ray.cor.15@gmail.com>
2026-09-18 23:23:36 -0600

    feat: clear main

--- src/main/java/com/test_vocacional/Main.java
+++ src/main/java/com/test_vocacional/Main.java
@@ -1,23 +1,14 @@
 package com.test_vocacional;
 
-import com.test_vocacional.view.Bienvenida;
+import com.test_vocacional.model.Estudiante;
+import com.test_vocacional.view.BienvenidaView;
+import com.test_vocacional.view.ReporteView;
+import com.test_vocacional.view.ResultadoView;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
-        Bienvenida bienvenida = new Bienvenida();
+        BienvenidaView bienvenida = new BienvenidaView();
         bienvenida.setVisible(true);
-
-//        Resultado resultado = new Resultado();
-//        resultado.setVisible(true);
-
-//        Estudiante estudiante = new Estudiante("Benjamin", 18, 5, "C", "Soporte");
-//
-//        Reporte reporte = new Reporte(estudiante);
-//        reporte.setVisible(true);
-
     }
-
 }
